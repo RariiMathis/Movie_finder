d56e4ca6760832eda44c502d88230aacc31615a0
Sackett <skeesen8@gmail.com>
2023-11-06 14:44:08 -0600

    new branch

--- lib/models/actors.py
+++ lib/models/actors.py
@@ -21,3 +21,9 @@ class Actors:
         sql = 'Select * FROM actors'
         list_of_tuples = CURSOR.execute(sql).fetchall()
         return [Actors.from_db(row) for row in list_of_tuples]
+
+    @classmethod
+    def from_db( cls, row_tuple ):
+        zoo_instance = Zoo( row_tuple[1], row_tuple[2] )
+        zoo_instance.id = row_tuple[0]
+        return zoo_instance
