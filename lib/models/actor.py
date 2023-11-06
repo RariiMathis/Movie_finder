07c466117d988d8c64f128999b616b79d6f153c5
Sackett <skeesen8@gmail.com>
2023-11-06 15:01:03 -0600

    added class method all and from_db to get a list of all actors

--- lib/models/actor.py
+++ lib/models/actor.py
@@ -1,8 +1,8 @@
 from models.__init__ import CURSOR, CONN
 
 
-class Actors:
-    all = []
+class Actor:
+    # all = []
 
     def __init__(self, name, age, origin, oscars, id = None):
         self.name = name
@@ -10,20 +10,21 @@ class Actors:
         self.origin = origin
         self.oscars = oscars
         self.id = id
-        Actors.all.append(self)
+        # Actor.all.append(self)
 
-    def all_actors(self):
-        return [actor for actor in Actors.all]
 
 
     @classmethod
     def all(cls):
         sql = 'Select * FROM actors'
         list_of_tuples = CURSOR.execute(sql).fetchall()
-        return [Actors.from_db(row) for row in list_of_tuples]
+        return [Actor.from_db(row) for row in list_of_tuples]
 
     @classmethod
     def from_db( cls, row_tuple ):
-        zoo_instance = Zoo( row_tuple[1], row_tuple[2] )
-        zoo_instance.id = row_tuple[0]
-        return zoo_instance
+        actor_instance = Actor( row_tuple[1], row_tuple[2], row_tuple[3], row_tuple[4] )
+        actor_instance.id = row_tuple[0]
+        return actor_instance
+
+
+    
