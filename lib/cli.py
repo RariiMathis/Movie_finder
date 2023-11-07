82eee9a4fe37b5f0e1108746b32bdbf20cb3f3b6
Sackett <skeesen8@gmail.com>
2023-11-07 10:30:19 -0600

    added delete classmethod and built cli to execute delete by actor id

--- lib/cli.py
+++ lib/cli.py
@@ -4,7 +4,8 @@ from helpers import (
     exit_program,
     helper_1,
     get_all_actors,
-    get_all_movies
+    get_all_movies,
+    delete_actor
 )
 
 
@@ -18,15 +19,30 @@ def main():
             get_all_actors()
         elif choice == ":)":
             get_all_movies()
+        elif choice == "3":
+            remove_actor()
         else:
             print("Invalid choice")
 
+def remove_actor():
+    while True:
+        menu()
+        get_all_actors()
+        choice = input ("> Enter Actor ID to delete here or 0 to go back -->")
+        if choice == "0":
+            main()
+        # if choice == (self.id):
+        delete_actor(choice)
+        
+
+        
 
 def menu():
     print("Please select an option:")
     print("0. Exit the program")
     print("1. List All Actors")
-    print(":) List All Movies")
+    print("2. List All Movies")
+    print("3. Delete Actor")
 
 
 if __name__ == "__main__":
